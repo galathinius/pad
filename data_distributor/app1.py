cc8377d04a840ec79b2d72cc9db1955056cbcc68
galathinius <anisoara.ionela@gmail.com>
2022-01-08 19:41:50 +0200

    add chunk distribution functionality

--- data_distributor/app1.py
+++ data_distributor/app1.py
@@ -6,11 +6,15 @@ from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://root:pass12345@mongo1:27017/myDatabase"
+app.config["MONGO_URI"] = "mongodb://root:pass12345@mongo1:27017"
 mongo = PyMongo(app)
 
 # if __name__ == '__main__':
 #       app.run(host='0.0.0.0', port=8000)
+
+if 'foo' not in mongo.database_names():
+    # create db foo
+    pass
 
 
 @app.route('/')
