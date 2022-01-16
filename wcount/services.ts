d9cdfc0a5dbf32fbf0753f7b2de4e75d4ee837ef
galathinius <anisoara.ionela@gmail.com>
2022-01-16 16:27:26 +0200

    add cache

--- wcount/services.ts
+++ wcount/services.ts
@@ -1,22 +1,39 @@
 import axios from "axios";
+import redis from "redis";
 
-export const get_text = (id: any) => {
-  const request = axios.get("http://distributor:8000/items/" + id);
-  // .then(function (response) {
-  //   // handle success
-  //   //   let text =;
+import { createClient } from "redis";
 
-  //   console.log(response.data.text);
-  //   return response.data.text;
-  // })
-  // .catch(function (error) {
-  //   // handle error
-  //   console.log(error);
-  // })
-  // .then(function () {
-  //   // always executed
-  // });
-  return request.then(function (response) {
-    return response.data.text;
-  });
+const client = createClient({
+  url: "redis://cache:6379",
+});
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+export const get_text = async (id: any) => {
+  await client.connect();
+  const value = await client.get(id);
+  if (value) {
+    console.log("got response from cache " + value);
+    return value;
+  } else {
+    const request = axios.get("http://distributor:8000/items/" + id);
+    // .then(function (response) {
+    //   // handle success
+    //   //   let text =;
+
+    //   console.log(response.data.text);
+    //   return response.data.text;
+    // })
+    // .catch(function (error) {
+    //   // handle error
+    //   console.log(error);
+    // })
+    // .then(function () {
+    //   // always executed
+    // });
+    return request.then(function (response) {
+      const text = response.data.text;
+      client.set(id, text);
+      return text;
+    });
+  }
 };
